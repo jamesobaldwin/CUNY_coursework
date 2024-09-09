2590f22d5da7e9e0d630674f16f38ab2e09741d2
James Baldwin <jbaldwin@gradcenter.cuny.edu>
2024-09-09 14:43:35 -0400

    ML HW1 Problem 1, Problem 2 start

--- ML/hw/ComputeMatrices.py
+++ ML/hw/ComputeMatrices.py
@@ -8,15 +8,22 @@ import matplotlib.pyplot as plt
 # first function to fill, compute distance matrix using loops
 def compute_distance_naive(X):
     N = X.shape[0]      # num of rows
+#    print("N: ", N)
     D = X[0].shape[0]   # num of cols
-    
+#    print("Shape of X[0]: ", X[0].shape)
+#    print("D: ", D)
+
     M = np.zeros([N,N])
     for i in range(N):
         for j in range(N):
             xi = X[i,:]
-            xj = X[i,:]
-            dist = 0.0  # a placetaker line, 
-                        # you have to change it to distance between xi and xj
+            xj = X[j,:]
+            '''
+            Euclidean Distance is defined by 
+            || x_i - x_j || = square root of the sum of the square of the difference between all the points
+            
+            '''
+            dist = np.sqrt(np.sum((xi - xj)**2))     # you have to change it to distance between xi and xj
             M[i,j] = dist
 
     return M
@@ -27,8 +34,18 @@ def compute_distance_smart(X):
     D = X[0].shape[0]  # num of cols
     
     # use X to create M
-    M = np.zeros([N, N])
+    M = np.zeros([N, N]) 
+
+    # compute the sum of squares for each row (i.e., ||x_i||^2 for all i)
+    X_squared = np.sum(X**2, axis=1).reshape(-1, 1)  # Shape becomes (N, 1)
     
+    # use Euclidean distance formula without loops
+    # ||x_i - x_j||^2 = ||x_i||^2 + ||x_j||^2 - 2 * x_i * x_j
+    M = X_squared + X_squared.T - ( 2 * np.dot( X, X.T ) )
+    
+    # take the square root to get the actual Euclidean distance
+    M = np.sqrt(np.maximum(M,0))
+
     return M
 
 # third function to fill, compute correlation matrix using loops
@@ -39,15 +56,25 @@ def compute_correlation_naive(X):
     # use X to create M
     M = np.zeros([D, D])
 
-    M = np.zeros([D, D])
+    # create a vector to store standard deviations 
+    sigmas = np.zeros(D)
+
     for i in range(D):
         for j in range(D):
             xi = X[:, i]
             xj = X[:, j]
-            corr = 0.0  # a placetaker line,
-            # you have to change it to correlation between xi and xj
-            M[i, j] = corr
+            
+            # calulate the mean of the row vector x_i, calculated across N rows 
+            mui = (1 / N) * np.sum(xi)
+            muj = (1 / N) * np.sum(xj)
+            # calculate s_ij 
+            M[i][j] = (1 / (N - 1)) * np.sum((xi - mui) * (xj - muj)) 
+            # store the standard deviations in a vector
+            if i == j:
+                sigmas[i] = np.sqrt( M[i][j] )
 
+            M[i][j] /= (sigmas[i]*sigmas[j])
+            # you have to change it to correlation between xi and xj
     return M
 
 # fourth function to fill, compute correlation matrix without loops
@@ -57,11 +84,12 @@ def compute_correlation_smart(X):
 
     # use X to create M
     M = np.zeros([D, D])
+    print("This is a test") 
 
     return M
 
 def main():
-    print 'starting comparing distance computation .....'
+    print('starting comparing distance computation .....')
     np.random.seed(100)
     params = range(10,141,10)   # different param setting
     nparams = len(params)       # number of different parameters
@@ -76,7 +104,7 @@ def main():
     for ncols in params:
         nrows = ncols * 10
 
-        print "matrix dimensions: ", nrows, ncols
+        print( "matrix dimensions: ", nrows, ncols )
 
         for i in range(10):
             X = np.random.rand(nrows, ncols)   # random matrix
@@ -99,21 +127,21 @@ def main():
             corr_loop = compute_correlation_naive(X)
             et = time.time()
             perf_corr_loop[i,counter] = et - st              # time difference
-
+ 
             st = time.time()
             corr_cool = compute_correlation_smart(X)
             et = time.time()
             perf_corr_cool[i,counter] = et - st
-
+ 
             assert np.allclose(corr_loop, corr_cool, atol=1e-06) # check if the two computed matrices are identical all the time
-
+ 
         counter = counter + 1
-
+ 
     mean_dist_loop = np.mean(perf_dist_loop, axis = 0)    # mean time for each parameter setting (over 10 trials)
     mean_dist_cool = np.mean(perf_dist_cool, axis = 0)
     std_dist_loop = np.std(perf_dist_loop, axis = 0)      # standard deviation
     std_dist_cool = np.std(perf_dist_cool, axis = 0)
-
+ 
     plt.figure(1)
     plt.errorbar(params, mean_dist_loop[0:nparams], yerr=std_dist_loop[0:nparams], color='red',label = 'Loop Solution for Distance Comp')
     plt.errorbar(params, mean_dist_cool[0:nparams], yerr=std_dist_cool[0:nparams], color='blue', label = 'Matrix Solution for Distance Comp')
@@ -123,13 +151,13 @@ def main():
     plt.legend()
     plt.savefig('CompareDistanceCompFig.pdf')
     # plt.show()    # uncomment this if you want to see it right way
-    print "result is written to CompareDistanceCompFig.pdf"
-
+    print("result is written to CompareDistanceCompFig.pdf")
+ 
     mean_corr_loop = np.mean(perf_corr_loop, axis = 0)    # mean time for each parameter setting (over 10 trials)
     mean_corr_cool = np.mean(perf_corr_cool, axis = 0)
     std_corr_loop = np.std(perf_corr_loop, axis = 0)      # standard deviation
     std_corr_cool = np.std(perf_corr_cool, axis = 0)
-
+ 
     plt.figure(2)
     plt.errorbar(params, mean_corr_loop[0:nparams], yerr=std_corr_loop[0:nparams], color='red',label = 'Loop Solution for Correlation Comp')
     plt.errorbar(params, mean_corr_cool[0:nparams], yerr=std_corr_cool[0:nparams], color='blue', label = 'Matrix Solution for Correlation Comp')
@@ -139,6 +167,6 @@ def main():
     plt.legend()
     plt.savefig('CompareCorrelationCompFig.pdf')
     # plt.show()    # uncomment this if you want to see it right way
-    print "result is written to CompareCorrelationCompFig.pdf"
-
+    print("result is written to CompareCorrelationCompFig.pdf")
+ 
 if __name__ == "__main__": main()
